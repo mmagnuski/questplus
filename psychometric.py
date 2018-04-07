5530bc3868896977a08d8ac2001cc748ea246782
mmagnuski <mmagnuski@swps.edu.pl>
2018-04-07 18:42:43 +0200

    ENH: better indentation for weibull

--- psychometric.py
+++ psychometric.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 def weibull(x, params, corr_at_thresh=0.75, chance_level=0.5):
-        # unpack params
-        if len(params) == 3:
-            t, b, lapse = params
-        else:
-            t, b = params
-            lapse = 0.
+    # unpack params
+    if len(params) == 3:
+        t, b, lapse = params
+    else:
+        t, b = params
+        lapse = 0.
 
-        k = ( -np.log((1.0 - corr_at_thresh) / (1.0 - chance_level)) ) \
-            ** (1.0 / b)
-        expo = ((k * x) / t) ** b
+    k = ( -np.log((1.0 - corr_at_thresh) / (1.0 - chance_level)) ) \
+        ** (1.0 / b)
+    expo = ((k * x) / t) ** b
 
-        return (1 - lapse) - (1 - lapse - chance_level) * np.exp(-expo)
+    return (1 - lapse) - (1 - lapse - chance_level) * np.exp(-expo)
 
 
 def weibull_db(contrast, params, guess=0.5):
